33660c23d5ac038343abf7d586403df2e2ca0768
badogan <basri.dogan.71@gmail.com>
2019-11-26 17:03:58 +0000

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,13 @@
+
+#require_relative './chatty_student.rb'
+
+class Student
+
+    def hello
+        puts "Hey there! I'm so excited to learn stuff.\n"
+    end
+
+    def raise_hand
+        puts "Pick me!\n"
+    end
+end
